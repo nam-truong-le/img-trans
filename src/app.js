a8f03f99ae69a3242e6c9bba8d64924994f7a667
Le, Nam Truong <nam.truong.le@sap.com>
2018-01-23 00:56:17 +0100

    added to app

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@
 const inquirer = require("inquirer");
 const winston = require("winston");
 const chalk = require("chalk");
-const path = require("path");
+const migration = require("./migration");
 
 (async () => {
     try {
-        const { sourceDirectory, targetDirectory } = await inquirer.prompt([{
+        const { sourceDirectory, targetDirectory, move } = await inquirer.prompt([{
             type: "input",
             name: "sourceDirectory",
             message: "Source directory"
@@ -15,7 +15,14 @@ const path = require("path");
             type: "input",
             name: "targetDirectory",
             message: "Target directory"
+        }, {
+            type: "confirm",
+            name: "move",
+            message: "Move",
+            default: true
         }]);
+
+        await migration.migrate(sourceDirectory, targetDirectory, move);
 
     } catch (error) {
         winston.info(chalk.red(error));
