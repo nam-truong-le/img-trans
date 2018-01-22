7192720c4a4ded3135cc55c548bcca7db0b24e9f
Le, Nam Truong <nam.truong.le@sap.com>
2018-01-23 00:46:34 +0100

    handle existing files

--- src/migration.js
+++ src/migration.js
@@ -7,7 +7,39 @@ const fs = require("fs-extra");
 const metadata = require("./metadata");
 const path = require("path");
 
+const moveFile = (file, targetFile, move) => {
+    winston.info(chalk.green(`   targeting '${targetFile}'...`));
+
+    if (fs.existsSync(targetFile)) {
+        winston.info(chalk.green(`   ${targetFile} exists`));
+        const fileBuff = fs.readFileSync(file);
+        const targetFileBuff = fs.readFileSync(targetFile);
+        if (fileBuff.equals(targetFileBuff)) {
+            // files are same
+            winston.info(chalk.green("   files are same"));
+            if (move) {
+                winston.info(chalk.green(`   source file '${file}' will be removed`));
+                fs.removeSync(file);
+            }
+        } else {
+            const extension = path.extname(targetFile);
+            const newTargetFile = path.join(path.dirname(targetFile), `${path.basename(targetFile, extension)}_diff${extension}`);
+            // files are different but same name
+            winston.info(chalk.green(`   rename to ${newTargetFile}`));
+            moveFile(file, newTargetFile);
+        }
+    } else {
+        if (move) {
+            fs.moveSync(file, targetFile);
+        } else {
+            fs.copySync(file, targetFile);
+        }
+    }
+};
+
 const migrateSingleFile = (file, targetDirectory, move) => {
+    winston.info(chalk.blue(`Move '${file}' to ${targetDirectory}`));
+
     const createdDate = metadata.createdDate(file);
     const year = createdDate.year();
     const month = createdDate.month();
@@ -16,11 +48,8 @@ const migrateSingleFile = (file, targetDirectory, move) => {
     const finalDirectory = path.join(targetDirectory, `${year}`, `${month + 1}`, `${date}`);
     fs.ensureDirSync(finalDirectory);
 
-    if (move) {
-        fs.moveSync(file, path.join(finalDirectory, path.basename(file)));
-    } else {
-        fs.copySync(file, path.join(finalDirectory, path.basename(file)));
-    }
+    const targetFile = path.join(finalDirectory, path.basename(file));
+    moveFile(file, targetFile, move);
 };
 
 const migrate = async (sourceDirectory, targetDirectory, move = true) => {
@@ -30,7 +59,6 @@ const migrate = async (sourceDirectory, targetDirectory, move = true) => {
         walk(sourceDirectory)
             .on("data", item => {
                 if (item.stats.isFile()) {
-                    winston.info(chalk.gray(item.path));
                     migrateSingleFile(item.path, targetDirectory, move);
                 }
             })
