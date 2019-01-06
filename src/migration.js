86e1f54d8a2fb1813aef30744d6400fd94fb1dee
Le, Nam Truong <lenamtruong@gmail.com>
2019-01-06 13:16:38 +0100

    rewrite migration

--- src/migration.js
+++ src/migration.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const walk = require("klaw");
 const winston = require("winston");
 const chalk = require("chalk");
-const fs = require("fs-extra");
 const metadata = require("./metadata");
 const path = require("path");
-const PQueue = require("p-queue");
-const queue = new PQueue({concurrency: 1});
 const fileUtils = require("./file");
 
-const migrateSingleFile = async (file, targetDirectory, move) => {
+module.exports = {
+    migrate
+};
+
+async function migrateSingleFile (file, targetDirectory, move) {
     winston.info(chalk.blue(`Move '${file}' to ${targetDirectory}`));
 
     try {
@@ -20,34 +20,20 @@ const migrateSingleFile = async (file, targetDirectory, move) => {
         const date = createdDate.date();
 
         const finalDirectory = path.join(targetDirectory, `${year}`, `${month + 1}`, `${date}`);
-        fs.ensureDirSync(finalDirectory);
 
         const targetFile = path.join(finalDirectory, path.basename(file));
         fileUtils.moveFile(file, targetFile, move);
     } catch (error) {
         winston.warn(chalk.yellow(`   skipped because of '${error}'`));
     }
-};
+}
 
-const migrate = async (sourceDirectory, targetDirectory, move = true) => {
-    fs.ensureDirSync(targetDirectory);
-
-    return new Promise((resolve, reject) => {
-        walk(sourceDirectory)
-            .on("data", async item => {
-                if (item.stats.isFile() && !path.basename(item.path).startsWith("._")) {
-                    queue.add(() => migrateSingleFile(item.path, targetDirectory, move));
-                } else {
-                    winston.info(`ignore file '${item.path}'`);
-                }
-            })
-            .on("end", () => {
-                queue.onEmpty().then(resolve);
-            })
-            .on("error", reject);
+async function migrate (sourceDirectory, targetDirectory, move = true) {
+    await fileUtils.processFiles(sourceDirectory, filePath => function(done) {
+        migrateSingleFile(filePath, targetDirectory, move).then(() => {
+            done();
+        }).catch(error => {
+            done(error || "Unknown Error");
+        });
     });
-};
-
-module.exports = {
-    migrate
-};
+}
--- test/migration.spec.js
+++ test/migration.spec.js
@@ -1,17 +1,44 @@
 "use strict";
 
+const chai = require("chai");
+chai.should();
 const migration = require("../src/migration");
 const path = require("path");
 const fs = require("fs-extra");
 
+const TEMP_DIR = path.join(__dirname, "__TMP__");
+
 describe("migration", () => {
+
+    beforeEach(() => {
+        fs.removeSync(TEMP_DIR);
+    });
+
+    afterEach(() => {
+        fs.removeSync(TEMP_DIR);
+    });
+
     it("test", async function() {
-        this.timeout(10000);
+        fs.copySync(path.join(__dirname, "data/DSC04815.jpg"), path.join(TEMP_DIR, "source", "DSC04815.jpg"));
+        await migration.migrate(path.join(TEMP_DIR, "source"), path.join(TEMP_DIR, "target"));
 
-        const source = path.join(__dirname, "data");
-        const target = path.join(__dirname, "data_migrated");
-        fs.removeSync(target);
+        fs.existsSync(path.join(TEMP_DIR, "source", "DSC04815.jpg")).should.equal(false);
+        fs.existsSync(path.join(TEMP_DIR, "target", "2017", "2", "14", "DSC04815.jpg")).should.equal(true);
+    });
 
-        await migration.migrate(source, target, false);
+    it("don't move", async function() {
+        fs.copySync(path.join(__dirname, "data/DSC04815.jpg"), path.join(TEMP_DIR, "source", "DSC04815.jpg"));
+        await migration.migrate(path.join(TEMP_DIR, "source"), path.join(TEMP_DIR, "target"), false);
+
+        fs.existsSync(path.join(TEMP_DIR, "source", "DSC04815.jpg")).should.equal(true);
+        fs.existsSync(path.join(TEMP_DIR, "target", "2017", "2", "14", "DSC04815.jpg")).should.equal(true);
+    });
+
+    it("not valid", async function() {
+        fs.copySync(path.join(__dirname, "data/perfect.pdf"), path.join(TEMP_DIR, "source", "perfect.pdf"));
+        await migration.migrate(path.join(TEMP_DIR, "source"), path.join(TEMP_DIR, "target"));
+
+        fs.existsSync(path.join(TEMP_DIR, "source", "perfect.pdf")).should.equal(true);
+        fs.existsSync(path.join(TEMP_DIR, "target", "2017", "2", "14", "perfect.pdf")).should.equal(false);
     });
 });
